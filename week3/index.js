9a1b62c99e90af18da9c745cbd04f61491d35046
Morgue16 <45204675+Morgue16@users.noreply.github.com>
2020-09-12 22:35:16 -0400

    Update index.js

--- week3/index.js
+++ week3/index.js
@@ -1,8 +1,6 @@
-// Import Article
-import Article from './Article.js';
-
+const Article = require("./Article.js");
 // Pass the values to the constructor method of Article
-const a = new Article (
+let a = new Article (
   {
     title: "This is the title!",
     author: "Me",
